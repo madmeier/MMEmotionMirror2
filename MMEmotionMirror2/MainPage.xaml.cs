d9156be2f331a3183c7ea969312951f590de7cf4
Christian Meier <christian.r.meier@gmail.com>
2016-05-09 20:42:01 +0200

    Some refinements

--- MMEmotionMirror2/MainPage.xaml.cs
+++ MMEmotionMirror2/MainPage.xaml.cs
@@ -16,12 +16,15 @@ using Windows.UI.Xaml.Media.Imaging;
 
 using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
+
 using Windows.Storage;
 using Windows.Storage.Streams;
 using System.Threading.Tasks;
 
 using Microsoft.ProjectOxford.Emotion;
 using Microsoft.ProjectOxford.Emotion.Contract;
+
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -38,20 +41,28 @@ namespace MMEmotionMirror2
         private DispatcherTimer timer;
 
 
+
+
+
+
         public MainPage()
         {
             this.InitializeComponent();
             initCamera();
 
-
+ 
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(10000);
             timer.Tick += takePhoto;
             timer.Start();
 
 
+
         }
 
+        /// <summary>
+        /// Initializes the camera and displays preview
+        /// </summary>
         private async void initCamera()
         {
             // Disable all buttons until initialization completes
@@ -83,6 +94,11 @@ namespace MMEmotionMirror2
             }
         }
 
+        /// <summary>
+        /// Callback in case there is a media capture error
+        /// </summary>
+        /// <param name="currentCaptureObject"></param>
+        /// <param name="currentFailure"></param>
         private  void mediaCapture_Failed(MediaCapture currentCaptureObject, MediaCaptureFailedEventArgs currentFailure)
         {
              status.Text = "MediaCaptureFailed: " + currentFailure.Message;
@@ -90,18 +106,16 @@ namespace MMEmotionMirror2
 
 
 
-        private Stream openStream(String fileName)
-        {
-            return File.OpenRead(fileName);
-        }
 
-
+        /// <summary>
+        /// Interface with project oxford
+        /// </summary>
+        /// <param name="imageFilePath"></param>
+        /// <returns></returns>
         private async Task<Emotion[]> UploadAndDetectEmotions(string imageFilePath)
         {
             string subscriptionKey = "xxx";
             EmotionServiceClient emotionServiceClient = new EmotionServiceClient(subscriptionKey);
-
-            //            FileStream imageFileStream = File.OpenRead(imageFilePath);
 
             Stream imageFileStream = await Task.Run(() => openFile(imageFilePath));
             Emotion[] emotionResult = await emotionServiceClient.RecognizeAsync(imageFileStream);
@@ -109,6 +123,11 @@ namespace MMEmotionMirror2
         }
 
 
+        /// <summary>
+        /// A little method to allows async stream opening
+        /// </summary>
+        /// <param name="imageFilePath"></param>
+        /// <returns></returns>
         private FileStream openFile(string imageFilePath)
         {
 
@@ -116,6 +135,12 @@ namespace MMEmotionMirror2
             return imageFileStream;
         }
 
+
+       /// <summary>
+       /// callbak to take a photo, store it and pass it to oxford for emotional analysis
+       /// </summary>
+       /// <param name="sender"></param>
+       /// <param name="e"></param>
        private async void takePhoto(object sender, object e)
         {
             try
@@ -142,23 +167,86 @@ namespace MMEmotionMirror2
                 {
                     foreach (Emotion emotion in emotionResult)
                     {
+
+
+                        float maxEmotion = 0.0F;
+                        int maxEmotionType = -1;
+
                         AngerSlide.Value = 100 * emotion.Scores.Anger;
+                        if (emotion.Scores.Anger > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Anger;
+                            maxEmotionType = 0;
+                        }
+
                         ContemptSlide.Value = 100 * emotion.Scores.Contempt;
+                        if (emotion.Scores.Contempt > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Contempt;
+                            maxEmotionType = 1;
+                        }
 
                         DisgustSlide.Value = 100 * emotion.Scores.Disgust;
-                        FearSlide.Value = 100 * emotion.Scores.Fear;
-                        HappinessSlide.Value = 100 * emotion.Scores.Happiness;
-                        NeutralSlide.Value = 100 * emotion.Scores.Neutral;
-                        SadnessSlide.Value = 100 * emotion.Scores.Sadness;
-                        SurpriseSlide.Value = 100 * emotion.Scores.Surprise;
+                        if (emotion.Scores.Disgust > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Disgust;
+                            maxEmotionType = 2;
+                        }
 
+                        FearSlide.Value = 100 * emotion.Scores.Fear;
+                        if (emotion.Scores.Fear > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Fear;
+                            maxEmotionType = 3;
+                        }
+
+                        HappinessSlide.Value = 100 * emotion.Scores.Happiness;
+                        if (emotion.Scores.Happiness > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Happiness;
+                            maxEmotionType = 4;
+                        }
+
+                        NeutralSlide.Value = 100 * emotion.Scores.Neutral;
+                        if (emotion.Scores.Neutral > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Neutral;
+                            maxEmotionType = 5;
+                        }
+
+                        SadnessSlide.Value = 100 * emotion.Scores.Sadness;
+                        if (emotion.Scores.Sadness > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Sadness;
+                            maxEmotionType = 6;
+                        }
+
+                        SurpriseSlide.Value = 100 * emotion.Scores.Surprise;
+                        if (emotion.Scores.Surprise > maxEmotion)
+                        {
+                            maxEmotion = emotion.Scores.Surprise;
+                            maxEmotionType = 7;
+                        }
 
                         emotionResultCount++;
+
+                        status.Text = "max = " + maxEmotion + " at " + maxEmotionType;
+
                     }
- 
+
                 }
                 else
                 {
+                    AngerSlide.Value = 0;
+                    ContemptSlide.Value = 0;
+                    DisgustSlide.Value = 0;
+                    FearSlide.Value = 0;
+                    HappinessSlide.Value = 0;
+                    NeutralSlide.Value =0;
+                    SadnessSlide.Value = 0;
+                    SurpriseSlide.Value = 0;
+
+
                     status.Text = "no emotions detected";
                 }
 
@@ -172,6 +260,10 @@ namespace MMEmotionMirror2
             {
             }
         }
+
+
+
+
 
     }
 }
